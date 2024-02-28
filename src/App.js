361c56e92d04842d44b4a4ef91841b329a52eea7
DeusLoVults <46011298+DeusLoVults@users.noreply.github.com>
2024-02-27 21:18:11 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from 'react-router-dom'
 
 import Home from './views/home'
-import NotFound from './views/not-found'
 
 function App() {
     return (
